faa61c82e635e8dca7d5fa774e1d974965122cea
Bakary <bakaryseydou@gmail.com>
2021-12-17 00:56:06 +0000

    commentaire git_initial_setup.sh

--- git_initial_setup.sh
+++ git_initial_setup.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
+## apres creation du repertoire, se mettre dedans
+## puis lancer ce script pour automatiser les commits et push
 
+## Remarque : ce script est a lancer juste la premiere fois !!!
+##             apres, a chasue changement, lancer automate_push.sh
 git init
 
 git config --global user.name "Bakary"
